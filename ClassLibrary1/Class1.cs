ef550a84ba40fe48259ec773b412a74d31483fdc
ymp <ymp.dev1@gmail.com>
2023-06-19 16:01:09 +0300

    1

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -2,6 +2,11 @@
 {
     public class Class1
     {
+        public int Id { get; set; }
 
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedOnUtc { get; set; }
+        public DateTime ModifiedOnUtc { get; set; }
     }
 }
